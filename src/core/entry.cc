c6ec2c7b21f3c8f6e7f904f4b6a59d8d76c2854f
Florian Burger <florianburger03@gmail.com>
2024-11-15 22:41:36 +0100

    added console logo and debug console header decoration

--- src/core/entry.cc
+++ src/core/entry.cc
@@ -1,5 +1,26 @@
 #include "common.hh"
 #include "app.hh"
+#include "debug/debug.hh"
+
+// (static)console figures
+static std::string s_info = R""""(
+--------------------------------------------------------
+|  _____                     _                  .--,   |
+| |  __ \                   | |             ,.-( (o)\  |
+| | |__) |_ _ _ __ _ __ ___ | |_           /   .)/\ ') |
+| |  ___/ _` | '__| '__/ _ \| __|       .',./'/    )/  |
+| | |  | (_| | |  | | | (_) | |_     ()=///=))))==()   |
+| |_|   \__,_|_|  |_|  \___/ \__|      /               |
+--------------------------------------------------------
+
+Parrot: v0.0
+Window-API: GLFW
+Graphics-API: OpenGL
+)"""";
+static std::string s_debug_head =
+"================= parrot debug console =================";
+static std::string s_debug_foot =
+"========================================================";
 
 namespace Parrot {
     // setup (defined in client-app)
@@ -7,8 +28,11 @@ namespace Parrot {
 
     // launch
     void launch() {
+        cout << s_info << endl;
+        cout << s_debug_head << endl;
         App app(setup());
         app.run();
+        cout << s_debug_foot << endl;
     }
 }
 
