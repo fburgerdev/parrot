5470fc0991bc2932599f7e6270049d85f1b26443
Florian Burger <florianburger03@gmail.com>
2024-11-27 13:11:51 +0100

    added mesh loading using tiny_obj_loader

--- src/graphics/mesh.hh
+++ src/graphics/mesh.hh
@@ -1,11 +1,24 @@
 #pragma once
 #include "uuid.hh"
+#include "math/matrix.hh"
 
 namespace Parrot {
+	// Vertex
+	struct Vertex {
+		// position, normal, tex_coords
+		Vec3<float32> position = Vec3<float32>(0, 0, 0);
+		Vec3<float32> normal = Vec3<float32>(0, 0, -1);
+		Vec2<float32> tex_coords = Vec2<float32>(0, 0);
+	};
+
 	// Mesh
 	class Mesh : public UUIDObject {
 	public:
 		// Mesh
 		Mesh(const stdf::path& filepath);
+
+		// vertices, indices
+		List<Vertex> vertices;
+		List<uint> indices;
 	};
 }
