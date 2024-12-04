fcd88343ae9b1359375b28d0565fc1fd8a361409
Florian Burger <florianburger03@gmail.com>
2024-12-04 14:29:04 +0100

    seperated camera and camera_component and applied derived_component and component_config

--- src/ecs/components/camera.cc
+++ src/ecs/components/camera.cc
@@ -1,14 +1,10 @@
 #include "common.hh"
 #include "camera.hh"
+#include "debug/debug.hh"
 
 namespace Parrot {
-	// CameraComponent
-	CameraComponent::CameraComponent(Entity& entity, const PerspectiveCamera& camera)
-		: Component(entity), camera(camera) {}
-	CameraComponent::CameraComponent(Entity& entity, const OrthographicCamera& camera)
-		: Component(entity), camera(camera) {}
-
 	// calcProjectionMatrix
+	// :: perspective
 	Mat4x4<float32> PerspectiveCamera::calcProjectionMatrix(float32 aspect) const {
 		Mat4x4<float32> proj_mat = zeros<float32, 4>();
 		float32 tan_fov = 1.0F / std::tan(fov / 2);
@@ -27,6 +23,7 @@ namespace Parrot {
 		proj_mat.at(3, 2) = 1;
 		return proj_mat;
 	}
+	// :: orthographic
 	Mat4x4<float32> OrthographicCamera::calcProjectionMatrix(float32 aspect) const {
 		Mat4x4<float32> proj_mat = zeros<float32, 4>();
 		proj_mat.at(0, 0) = 1.0F / scale;
@@ -44,9 +41,43 @@ namespace Parrot {
 		proj_mat.at(3, 3) = 1;
 		return proj_mat;
 	}
-	Mat4x4<float32> CameraComponent::calcProjectionMatrix(float32 aspect) const {
-		return std::visit([=](const auto& value) {
-			return value.calcProjectionMatrix(aspect);
-		}, camera);
+
+	// Camera
+	Camera::Camera(const PerspectiveCamera& value)
+		: value(value) {}
+	Camera::Camera(const OrthographicCamera& value)
+		: value(value) {}
+	// calcProjectionMatrix
+	Mat4x4<float32> Camera::calcProjectionMatrix(float32 aspect) const {
+		return std::visit([=](const auto& specific_camera) {
+			return specific_camera.calcProjectionMatrix(aspect);
+		}, value);
+	}
+	// getComponentID
+	usize Camera::getComponentID() const {
+		return typeid(Camera).hash_code();
+	}
+	// createComponent
+	UniquePtr<Component> Camera::createComponent(Entity& entity) const {
+		return std::make_unique<CameraComponent>(*this, entity);
+	}
+
+	// update
+	void CameraComponent::update([[maybe_unused]] float32 delta_time) {
+		LOG_ECS_TRACE("camera-component update: {}", *this);
+	}
+
+	// <<
+	ostream& operator<<(ostream& stream, const PerspectiveCamera& camera) {
+		return stream << "PerspectiveCamera(fov=" << camera.fov << ",zrange=" << camera.z_range << ')';
+	}
+	ostream& operator<<(ostream& stream, const OrthographicCamera& camera) {
+		return stream << "OrthographicCamera(scale=" << camera.scale << ",zrange=" << camera.z_range << ')';
+	}
+	ostream& operator<<(ostream& stream, const Camera& camera) {
+		std::visit([&](const auto& specific_camera) {
+			stream << specific_camera;
+		}, camera.value);
+		return stream;
 	}
 }
