0d940ab11b78ec86d1a4abffbae6c1d346695f10
Florian Burger <florianburger03@gmail.com>
2024-12-04 14:27:39 +0100

    added derived_component and component_config

--- src/ecs/component.cc
+++ src/ecs/component.cc
@@ -6,10 +6,6 @@ namespace Parrot {
 	// Component
 	Component::Component(Entity& entity)
 		: entity(entity) {}
-	// ~Component
-	Component::~Component() {
-		// do nothing
-	}
 	// update
 	void Component::update([[maybe_unused]] float32 update) {
 		LOG_ECS_TRACE("component update");
--- src/ecs/component.hh
+++ src/ecs/component.hh
@@ -11,7 +11,7 @@ namespace Parrot {
 		// Component
 		Component(Entity& entity);
 		// ~Component
-		virtual ~Component();
+		virtual ~Component() = default;
 		// update
 		virtual void update(float32 update);
 
@@ -23,4 +23,25 @@ namespace Parrot {
 	usize getComponentID() {
 		return typeid(T).hash_code();
 	}
+
+	// DerivedComponent
+	template<class T>
+	class DerivedComponent : public T, public Component {
+	public:
+		// DerivedComponent / ~DerivedComponent
+		DerivedComponent(const T& value, Entity& entity)
+			: T(value), Component(entity) {}
+		virtual ~DerivedComponent() = default;
+	};
+	// ComponentConfig
+	class ComponentConfig {
+	public:
+		// ~ComponentConfig
+		virtual ~ComponentConfig() = default;
+
+		// getComponentID
+		virtual usize getComponentID() const = 0;
+		// createComponent
+		virtual UniquePtr<Component> createComponent(Entity& entity) const = 0;
+	};
 }
