3ff8070c1d689f9b032e66f062d69988b0b3d271
Florian Burger <florianburger03@gmail.com>
2024-12-04 14:32:53 +0100

    added transform and component_config to entity_config and considered it in json parsing

--- src/formats/entity_config.hh
+++ src/formats/entity_config.hh
@@ -1,5 +1,8 @@
 #pragma once
 #include "uuid.hh"
+#include "ecs/transform.hh"
+#include "ecs/component_registry.hh"
+#include "debug/debug.hh"
 
 namespace Parrot {
 	// EntityConfig
@@ -20,7 +23,30 @@ namespace Parrot {
 			if (json.contains("tag")) {
 				tag = string(json.at("tag"));
 			}
-			// TODO: transform
+			// transform
+			if (json.contains("transform")) {
+				if (json.at("transform").contains("position")) {
+					transform.position = Vec3<float32>(
+						json.at("transform").at("position")[0],
+						json.at("transform").at("position")[1],
+						json.at("transform").at("position")[2]
+					);
+				}
+				if (json.at("transform").contains("rotation")) {
+					transform.rotation = Vec3<float32>(
+						json.at("transform").at("rotation")[0],
+						json.at("transform").at("rotation")[1],
+						json.at("transform").at("rotation")[2]
+					);
+				}
+				if (json.at("transform").contains("scale")) {
+					transform.scale = Vec3<float32>(
+						json.at("transform").at("scale")[0],
+						json.at("transform").at("scale")[1],
+						json.at("transform").at("scale")[2]
+					);
+				}
+			}
 			// children
 			if (json.contains("children")) {
 				for (const auto& child : json.at("children")) {
@@ -35,15 +61,23 @@ namespace Parrot {
 					}
 				}
 			}
-			// TODO: components
+			// components
+			if (json.contains("components")) {
+				for (const auto& [name, data] : json.at("components").items()) {
+					if (g_component_registry<JSON>.contains(name)) {
+						components.emplace_back(g_component_registry<JSON>.at(name)(data));
+					}
+					else {
+						LOG_ASSET_WARNING("unregistered component '{}' in json, will be ignored ", name);
+					}
+				}
+			}
 		}
 
 		// tag, transform, children, components
 		string tag = "Entity";
-		// TODO: uncomment
-		// Transform transform;
+		Transform transform;
 		List<Variant<uuid, stdf::path, EntityConfig>> children;
-		// TODO: uncomment
-		// Map<usize, Component> components;
+		List<UniquePtr<ComponentConfig>> components;
 	};
 }
