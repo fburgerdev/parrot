cfb30e544cc4d19818472117300d801eaac0d907
Florian Burger <florianburger03@gmail.com>
2024-12-09 15:17:58 +0100

    implemented example/demo controller and added cube mesh

--- example/demo/scripts/controller.hh
+++ example/demo/scripts/controller.hh
@@ -9,12 +9,75 @@ namespace Parrot {
 		PlayerController(Entity& entity)
 			: entity(entity) {}
 
+		// resolveEvent
+		virtual bool resolveEvent(const Event& e) override {
+			if (auto* kp = e.getKeyPress()) {
+				if (kp->state == KeyState::PRESSED || kp->state == KeyState::RELEASED) {
+					if (kp->code == KeyCode::KEY_W) {
+						_w = (kp->state == KeyState::PRESSED);
+					}
+					else if (kp->code == KeyCode::KEY_A) {
+						_a = (kp->state == KeyState::PRESSED);
+					}
+					else if (kp->code == KeyCode::KEY_S) {
+						_s = (kp->state == KeyState::PRESSED);
+					}
+					else if (kp->code == KeyCode::KEY_D) {
+						_d = (kp->state == KeyState::PRESSED);
+					}
+					else if (kp->code == KeyCode::KEY_SPACE) {
+						_space = (kp->state == KeyState::PRESSED);
+					}
+					else if (kp->code == KeyCode::KEY_LEFT_CONTROL) {
+						_ctrl = (kp->state == KeyState::PRESSED);
+					}
+				}
+				return true;
+			}
+			if (auto* mm = e.getMouseMove()) {
+				if (_mouse_pos) {
+					Vec2<> delta = (mm->coords - *_mouse_pos);
+					entity.transform.rotation.y -= _mouse_speed * delta.x;
+					entity.transform.rotation.x -= _mouse_speed * delta.y;
+					entity.transform.rotation.y = clamp(
+						entity.transform.rotation.y, -PI<> / 2, +PI<> / 2
+					);
+				}
+				_mouse_pos = mm->coords;
+			}
+			return false;
+		}
 		// onUpdate
 		virtual void onUpdate(float32 delta_time) override {
-			// controls...
+			auto rotation_matrix = calcRotationMatrix(entity.transform.rotation);
+			auto forward = Vec3<>(rotation_matrix * Vec4<>(0, 0, 1, 0));
+			auto up = Vec3<>(rotation_matrix * Vec4<>(0, 1, 0, 0));
+			auto right = Vec3<>(rotation_matrix * Vec4<>(1, 0, 0, 0));
+			if (_w) {
+				entity.transform.position += forward * _move_speed * delta_time;
+			}
+			if (_a) {
+				entity.transform.position -= right * _move_speed * delta_time;
+			}
+			if (_s) {
+				entity.transform.position -= forward * _move_speed * delta_time;
+			}
+			if (_d) {
+				entity.transform.position += right * _move_speed * delta_time;
+			}
+			if (_space) {
+				entity.transform.position += up * _move_speed * delta_time;
+			}
+			if (_ctrl) {
+				entity.transform.position -= up * _move_speed * delta_time;
+			}
 		}
 
 		// entity
 		Entity& entity;
+	private:
+		DefaultFloat _move_speed = 10.0, _mouse_speed = 0.001;
+		Opt<Vec2<>> _mouse_pos;
+		bool _w = false, _a = false, _s = false, _d = false, _space = false, _ctrl = false;
 	};
 }
