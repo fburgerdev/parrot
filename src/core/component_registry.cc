05001b82d027839ea897a0b913c98ee3f1edb6be
Florian Burger <florianburger03@gmail.com>
2024-12-15 17:17:49 +0100

    added light sources with json parsing and registered light_source as component

--- src/core/component_registry.cc
+++ src/core/component_registry.cc
@@ -2,6 +2,7 @@
 #include "registry.hh"
 #include "ecs/component.hh"
 #include "graphics/camera.hh"
+#include "graphics/light_source.hh"
 #include "graphics/render_object.hh"
 #include <nlohmann/json.hh>
 using json = nlohmann::json;
@@ -17,6 +18,15 @@ namespace Parrot {
 				typeid(DerivedComponentConfig<Camera>).hash_code(),
 				[](const json& json, const stdf::path& filepath) {
 					return std::make_unique<DerivedComponentConfig<Camera>>(json, filepath);
+				}
+			},
+		},
+		{
+			"light-source",
+			{
+				typeid(DerivedComponentConfig<LightSource>).hash_code(),
+				[](const json& json, const stdf::path& filepath) {
+					return std::make_unique<DerivedComponentConfig<LightSource>>(json, filepath);
 				}
 			},
 		},
---
+++ src/graphics/light_source.hh
@@ -0,0 +1,156 @@
+#pragma once
+#include "math/matrix.hh"
+
+namespace Parrot {
+	// AmbientLight
+	struct AmbientLight {
+		// intensity, color
+		DefaultFloat intensity = 1;
+		Vec3<uint8> color = { 255, 255, 255 };
+	};
+	// DirectionalLight
+	struct DirectionalLight {
+		// direction, intensity, color
+		Vec3<DefaultFloat> direction;
+		DefaultFloat intensity = 1;
+		Vec3<uint8> color = { 255, 255, 255 };
+	};
+	// PointLight
+	struct PointLight {
+		// position, range, intensity, color
+		Vec3<DefaultFloat> position;
+		DefaultFloat range, intensity = 1;
+		Vec3<uint8> color = { 255, 255, 255 };
+	};
+	// SpotLight
+	struct SpotLight {
+		// position, direction, angle, intensity, color
+		Vec3<DefaultFloat> position, direction;
+		DefaultFloat angle = PI<> / 2, intensity = 1;
+		Vec3<uint8> color = { 255, 255, 255 };
+	};
+
+	// LightSource
+	class LightSource {
+	public:
+		// LightSource
+		template<class JSON> requires(requires(JSON json) { json.at("key"); })
+		LightSource(const JSON& json, const stdf::path& filepath) {
+			loadFromJSON(json, filepath);
+		}
+		// loadFromJSON
+		template<class JSON> requires(requires(JSON json) { json.at("key"); })
+		void loadFromJSON(const JSON& json, [[maybe_unused]] const stdf::path& filepath) {
+			if (json.at("type") == "ambient") {
+				AmbientLight light;
+				// intensity
+				if (json.contains("intensity")) {
+					light.intensity = json.at("intensity");
+				}
+				// color
+				if (json.contains("color")) {
+					light.color = Vec3<uint8>(
+						json.at("color")[0],
+						json.at("color")[1],
+						json.at("color")[2]
+					);
+				}
+				value = light;
+			}
+			else if (json.at("type") == "directional") {
+				DirectionalLight light;
+				// direction
+				light.direction = Vec3<DefaultFloat>(
+					json.at("direction")[0],
+					json.at("direction")[1],
+					json.at("direction")[2]
+				);
+				// intensity
+				if (json.contains("intensity")) {
+					light.intensity = json.at("intensity");
+				}
+				// color
+				if (json.contains("color")) {
+					light.color = Vec3<uint8>(
+						json.at("color")[0],
+						json.at("color")[1],
+						json.at("color")[2]
+					);
+				}
+				value = light;
+			}
+			else if (json.at("type") == "point") {
+				PointLight light;
+				// position
+				light.position = Vec3<DefaultFloat>(
+					json.at("position")[0],
+					json.at("position")[1],
+					json.at("position")[2]
+				);
+				// range
+				if (json.contains("range")) {
+					light.range = json.at("range");
+				}
+				// intensity
+				if (json.contains("intensity")) {
+					light.intensity = json.at("intensity");
+				}
+				// color
+				if (json.contains("color")) {
+					light.color = Vec3<uint8>(
+						json.at("color")[0],
+						json.at("color")[1],
+						json.at("color")[2]
+					);
+				}
+				value = light;
+			}
+			else if (json.at("type") == "spot") {
+				SpotLight light;
+				// position
+				light.position = Vec3<DefaultFloat>(
+					json.at("position")[0],
+					json.at("position")[1],
+					json.at("position")[2]
+				);
+				// direction
+				light.direction = Vec3<DefaultFloat>(
+					json.at("direction")[0],
+					json.at("direction")[1],
+					json.at("direction")[2]
+				);
+				// angle
+				if (json.contains("angle")) {
+					light.angle = json.at("angle");
+				}
+				// intensity
+				if (json.contains("intensity")) {
+					light.intensity = json.at("intensity");
+				}
+				// color
+				if (json.contains("color")) {
+					light.color = Vec3<uint8>(
+						json.at("color")[0],
+						json.at("color")[1],
+						json.at("color")[2]
+					);
+				}
+				value = light;
+			}
+		}
+
+		// value
+		Variant<
+		  AmbientLight,
+		  DirectionalLight,
+		  PointLight,
+		  SpotLight
+		> value;
+	};
+	// <<
+	ostream& operator<<(ostream& stream, const AmbientLight& light);
+	ostream& operator<<(ostream& stream, const DirectionalLight& light);
+	ostream& operator<<(ostream& stream, const PointLight& light);
+	ostream& operator<<(ostream& stream, const SpotLight& light);
+	ostream& operator<<(ostream& stream, const LightSource& source);
+}
