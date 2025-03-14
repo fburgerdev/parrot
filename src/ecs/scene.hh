beabeb3d96faac06d7d465c98a97010234f73fda
Florian Burger <florianburger03@gmail.com>
2025-03-14 18:54:24 +0100

    feat(ecs): add non-const query-entities overload

--- src/ecs/scene.hh
+++ src/ecs/scene.hh
@@ -19,6 +19,22 @@ namespace Parrot {
 
     // queryEntities
     template<class T>
+    List<Entity*> queryEntities() {
+      List<Entity*> found;
+      Stack<Entity*> stack({ &root });
+      while (!stack.empty()) {
+        Entity* top = stack.top();
+        stack.pop();
+        if (top->hasComponent<T>()) {
+          found.push_back(top);
+        }
+        top->foreachChild([&](Entity& child) {
+          stack.push(&child);
+        });
+      }
+      return found;
+    }
+    template<class T>
     List<const Entity*> queryEntities() const {
       List<const Entity*> found;
       Stack<const Entity*> stack({ &root });
